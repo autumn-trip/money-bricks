50cc27c4e952c331963ab57ce44255461543e72c
Yeop9999 <pisa0123@naver.com>
2025-02-03 17:51:51 +0900

    Delete moneybricks0203 directory

--- moneybricks0203/src/main/java/com/moneybricks/service/moneynews/MoneynewsService.java
+++
@@ -1,8 +0,0 @@
-package com.moneybricks.service.moneynews;
-
-import com.moneybricks.dto.MoneynewsDTO;
-import java.util.List;
-
-public interface MoneynewsService {
-    List<MoneynewsDTO> getMoneyNews(String query, int page, int size);
-}
